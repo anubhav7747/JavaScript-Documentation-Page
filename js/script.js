d1ce0170d8219bc6634cfcf05d4553aaeb7d0f77
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-08-22 02:03:15 +0530

    added clock

--- js/script.js
+++ js/script.js
@@ -1,6 +1,31 @@
-const displayTime = () => {
-    let now = new Date();
-    document.getElementById('time').innerHTML = now;
-}
+// for displaying time
 
-setInterval(displayTime, 1000);
+// const displayTime = () => {
+//     let now = new Date();
+//     document.getElementById('time').innerHTML = now;
+// }
+
+// setInterval(displayTime, 1000);
+
+// const displayGreet = () => {
+//     alert('Thank you for visiting.');
+// }
+
+// setTimeout(displayGreet, 10000);
+
+setInterval(()=>{
+    let date = new Date();
+    let options = {
+      weekday: 'short', year: 'numeric', month: 'short',
+      day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit'
+    };
+  
+    let d = date.toLocaleTimeString('en-us', options);
+    time.innerHTML = d;
+}, 1000);
+
+
+// link to open in new tab
+// document.getElementById('linkOpenNewTab').addEventListener('click', function () {
+//   window.location = 'https://replit.com/@AnubhavTiwari7/24ScriptTag#index.html';
+// });
