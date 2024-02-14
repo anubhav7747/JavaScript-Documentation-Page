bae0d05a0139a2d6d177a50fd6cce9996cdfe921
anubhav7747 <anubhavtiwari007005@gmail.com>
2024-02-15 00:33:00 +0530

    mobile responsive

--- js/script.js
+++ js/script.js
@@ -24,6 +24,43 @@ setInterval(()=>{
     time.innerHTML = d;
 }, 1000);
 
+let menuBtn = document.querySelector(".menu");
+let mobileElement = document.querySelector(".mobile");
+let navElement = document.querySelector(".nav");
+
+
+menuOptions = () => {
+  // console.log('clicked');
+  menuBtn.innerHTML = '';
+  // menuBtn.classList.remove("menu");
+  // menuBtn.classList.add("undoChanges");
+  let img = document.createElement('img');
+  img.src = '/images/close.png';
+  img.width = 20;
+  menuBtn.appendChild(img);
+  mobileElement.style.border = "none";
+  navElement.style.display = "block";
+}
+
+
+// let undoChangesElement = document.querySelector(".undoChanges");
+
+// undoCha = () => {
+//   // console.log("undoChange");
+//   undoChangesElement.innerHTML = '';
+//   undoChangesElement.classList.add("menu");
+//   undoChangesElement.classList.remove("undoChanges");
+//   let hrElement = document.createElement('hr');
+//   undoChangesElement.appendChild(hrElement);
+//   undoChangesElement.appendChild(hrElement);
+//   undoChangesElement.appendChild(hrElement);
+//   mobileElement.style.border = "block";
+//   navElement.style.display = 'none';
+// }
+
+menuBtn.addEventListener("click", menuOptions);
+
+// undoChangesElement.addEventListener("click", undoCha);
 
 // link to open in new tab
 // document.getElementById('linkOpenNewTab').addEventListener('click', function () {
